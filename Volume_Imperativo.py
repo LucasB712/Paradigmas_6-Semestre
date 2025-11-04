58dadb7d865cfe7227fad1e5a5826f3ef804cfaf
Lucas Barreto <93289129+LucasB712@users.noreply.github.com>
2025-11-04 09:16:29 -0300

    Create Volume_Imperativo.py

---
+++ Volume_Imperativo.py
@@ -0,0 +1,14 @@
+
+pi = 3.14
+r = 13
+h = 10
+area_base = 5
+
+volume_esfera = 4 * pi * r * r  * r  * r / 3
+print(volume_esfera)
+
+volume_piramide = area_base * h / 3
+print(volume_piramide)
+
+volume_cilindro= pi * r * r  * h
+print(volume_cilindro)
