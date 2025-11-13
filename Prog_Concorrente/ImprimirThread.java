a03ea7d9b5999926c5522e87e943411aa8b36826
Lucas Barreto <93289129+LucasB712@users.noreply.github.com>
2025-11-13 11:12:14 -0300

    Create ImprimirThread.java

---
+++ Prog_Concorrente/ImprimirThread.java
@@ -0,0 +1,26 @@
+class ImprimirThread extends Thread{
+  
+  private int tempoEspera;
+  public ImprimirThread(String name, int tempoespera) {
+    super(name);
+    this.tempoEspera = tempoEspera;
+    
+    System.out.println("Nome da thread: " + getName() + " Dorme por " + tempoEspera + "ms");
+  
+  }
+
+  @Override
+  public void run() {
+    try {
+      
+      System.out.println(getName() + " indo dormir");
+      Thread.sleep(tempoEspera);
+      System.out.println("Escrevendo alguma noticia...");
+    } catch ( Exception exception) {
+      System.out.println("Thread interrompida: " + exception.toString());
+
+    }
+    
+    System.out.println(getName() + " ja dormiu e terminou execucao");
+  }
+}
